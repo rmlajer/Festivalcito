f680d9408bd42861aecdd0628e071c72bd91fc3d
morten-lund <morten2608@gmail.com>
2022-12-09 12:41:58 +0100

    Person rep done

--- Festivalcito/Server/Models/PersonRepositoryFolder/PersonRepository.cs
+++ Festivalcito/Server/Models/PersonRepositoryFolder/PersonRepository.cs
@@ -15,9 +15,27 @@ namespace Festivalcito.Server.Models.PersonRepositoryFolder
         public bool CreatePerson(Person person)
         {
             //Tager et Person object og indsætter via SQL statement i vores database
-            //Formatere time og float for at det passer med postgreSQL
+            //Formatere DateOfBirth for at det passer med postgreSQL 
             Console.WriteLine("CreatePerson - Repository");
-            var sql = "";
+            var sql = $"INSERT INTO public.personlist" +
+                $"(assigned, iscoordinator, emailadress, firstname, lastname, dateofbirth," +
+                $"address, postalcode, city, country, nationality, danishlevel, gender, membershippaid)" +
+                $"VALUES (" +
+                $"  {person.Assigned}," +
+                $"  {person.IsCoordinator}," +
+                $" '{person.Email}', " +
+                $" '{person.FirstName}'," +
+                $" '{person.LastName}'," +
+                $" '{person.DateOfBirth.ToString("yyyy-MM-dd HH:mm:ss").Replace("\"", "").Replace(".", ":")}', " +
+                $" '{person.Address}'," +
+                $" '{person.PostalCode}', " +
+                $" '{person.City}', " +
+                $" '{person.Country}', " +
+                $" '{person.Nationality}'," +
+                $"  {person.DanishLevel}, " +
+                $" '{person.Gender}', " +
+                $" '{person.MembershipPaid}'";
+
             Console.WriteLine("sql: " + sql);
 
             //Isolere "var connection" fra resten af scope ved brug af using
@@ -43,7 +61,7 @@ namespace Festivalcito.Server.Models.PersonRepositoryFolder
         public Person ReadPerson(int personId)
         {
             Console.WriteLine("ReadPerson");
-            var SQL = $"SELECT * from person WHERE personID = {personId}";
+            var SQL = $"SELECT * from public.personlist WHERE personID = {personId}";
 
             //Isolere "var connection" fra resten af scope ved brug af using
             //forsøger at eksikvere sql statement mod database
@@ -81,8 +99,25 @@ namespace Festivalcito.Server.Models.PersonRepositoryFolder
         {
 
             Console.WriteLine("UpdatePerson");
-            var sql = "";
+            var sql = $"UPDATE public.personlist SET " +
+                $"assigned= {person.Assigned}, " +
+                $"iscoordinator= {person.IsCoordinator}, " +
+                $"emailadress= '{person.Email}', " +
+                $"firstname= '{person.FirstName}', " +
+                $"lastname= '{person.LastName}', " +
+                $"dateofbirth={person.DateOfBirth.ToString("yyyy-MM-dd HH:mm:ss").Replace("\"", "").Replace(".", ":")}',  " +
+                $"address= '{person.Address}', " +
+                $"postalcode= '{person.PostalCode}', " +
+                $"city='{person.City}', " +
+                $"country='{person.Country}', " +
+                $"nationality='{person.Nationality}', " +
+                $"danishlevel={person.DanishLevel}, " +
+                $"gender='{person.Gender}', " +
+                $"membershippaid={person.MembershipPaid}'" +
+                $"WHERE personId = {person.PersonID}"; 
 
+
+      
             Console.WriteLine("sql: " + sql);
 
 
@@ -108,7 +143,7 @@ namespace Festivalcito.Server.Models.PersonRepositoryFolder
         {
 
             Console.WriteLine("DeletePerson");
-            var sql = $"DELETE FROM public.Person WHERE personid = {personId}";
+            var sql = $"DELETE FROM public.personlist WHERE personid = {personId}";
 
             //Isolere "var connection" fra resten af scope ved brug af using
             //forsøger at eksikvere sql statement mod database
@@ -121,7 +156,7 @@ namespace Festivalcito.Server.Models.PersonRepositoryFolder
                 }
                 catch
                 {
-                    Console.WriteLine("Couldn't update the Person from the list");
+                    Console.WriteLine("Couldn't Delete the Person from the list");
                     return false;
                 }
 
--- Festivalcito/Shared/Models/Person.cs
+++ Festivalcito/Shared/Models/Person.cs
@@ -9,7 +9,7 @@ namespace Festivalcito.Shared.Models
         public string? Email { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string? Address { get; set; }
         public string? PostalCode { get; set; }
         public string? City { get; set; }
